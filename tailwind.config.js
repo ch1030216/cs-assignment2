8bc2faeef278301b8e8aa96323d2be6a7ba3bb99
ch1030216 <ch1030216@gmail.com>
2026-05-30 17:12:25 +0900

    fix22

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // ⭐️ src 폴더 내부의 모든 js, jsx 파일을 빠짐없이 감시하도록 경로를 명확히 세팅합니다.
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
