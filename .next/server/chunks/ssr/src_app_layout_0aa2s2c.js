e676eda5b38272a5c69134403cdeba4172b43364
ch1030216 <ch1030216@gmail.com>
2026-05-30 22:17:58 +0900

    fixed

---
+++ .next/server/chunks/ssr/src_app_layout_0aa2s2c.js
@@ -0,0 +1,3 @@
+module.exports=[29735,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"ko",children:(0,b.jsx)("body",{className:"antialiased bg-slate-50 text-slate-900",children:a})})},"metadata",0,{title:"나만의 데스크톱 대시보드 플래너",description:"플래너와 다이어리를 한 눈에"}])},81967,a=>{a.n(a.i(29735))}];
+
+//# sourceMappingURL=src_app_layout_0aa2s2c.js.map
